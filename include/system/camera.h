df82639ad41389fb50cb581aebb0f2518f983bd0
thekraven <savethestyle1@gmail.com>
2012-03-31 00:02:04 -0500

    camera and offline charging revert
    Change-Id: If2a1f2a7374d51901e8eeca2f13672400728853e

--- include/system/camera.h
+++ include/system/camera.h
@@ -80,11 +80,18 @@ enum {
     CAMERA_MSG_RAW_IMAGE = 0x0080,        // dataCallback
     CAMERA_MSG_COMPRESSED_IMAGE = 0x0100, // dataCallback
     CAMERA_MSG_RAW_IMAGE_NOTIFY = 0x0200, // dataCallback
+#ifdef QCOM_HARDWARE 
+    CAMERA_MSG_META_DATA = 0x400, 
+    CAMERA_MSG_LOCK_MUTEX = 0x800,    //Get Mutex from HAL 
+    CAMERA_MSG_UNLOCK_MUTEX = 0x1000,    //Release Mutex from HAL 
+#endif 
     // Preview frame metadata. This can be combined with
     // CAMERA_MSG_PREVIEW_FRAME in dataCallback. For example, the apps can
     // request FRAME and METADATA. Or the apps can request only FRAME or only
     // METADATA.
     CAMERA_MSG_PREVIEW_METADATA = 0x0400, // dataCallback
+	CAMERA_MSG_STATS_DATA       = 0x800,
+	
     CAMERA_MSG_ALL_MSGS = 0xFFFF
 };
 
@@ -142,10 +149,25 @@ enum {
      * Stop the face detection.
      */
     CAMERA_CMD_STOP_FACE_DETECTION = 7,
+#ifdef QCOM_HARDWARE 
+    CAMERA_CMD_HISTOGRAM_ON = 8, 
+    CAMERA_CMD_HISTOGRAM_OFF = 9, 
+    CAMERA_CMD_HISTOGRAM_SEND_DATA = 10, 
+    CAMERA_CMD_FACE_DETECTION_ON = 7, 
+    CAMERA_CMD_FACE_DETECTION_OFF = 8, 
+    CAMERA_CMD_SEND_META_DATA = 9, 
+#endif 
 };
 
 /** camera fatal errors */
 enum {
+    CAMERA_SUPPORT_MODE_2D = 0x01, /* Camera Sensor supports 2D mode. */  
+    CAMERA_SUPPORT_MODE_3D = 0x02, /* Camera Sensor supports 3D mode. */  
+    CAMERA_SUPPORT_MODE_NONZSL = 0x04, /* Camera Sensor in NON-ZSL mode. */  
+    CAMERA_SUPPORT_MODE_ZSL = 0x08 /* Camera Sensor supports ZSL mode. */  
+};  
+  
+enum {  
     CAMERA_ERROR_UNKNOWN = 1,
     CAMERA_ERROR_SERVER_DIED = 100
 };
